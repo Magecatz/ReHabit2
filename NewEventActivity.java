e53b5e75f1997ceffba1cb8c1e6fe1fa7493ee3f
Magcatz <magecatz@gmail.com>
2018-01-20 22:45:26 -0500

    Added new event and stuff

--- NewEventActivity.java
+++ NewEventActivity.java
@@ -1,12 +1,16 @@
 package com.example.quinn.rehabit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
+
+import java.util.ArrayList;
 
 public class NewEventActivity extends AppCompatActivity {
 
@@ -17,11 +21,28 @@ public class NewEventActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        EditText et = (EditText) findViewById(R.id.editText);
+        /**EditText et = (EditText) findViewById(R.id.editText);
         String text= et.getEditableText().toString();
+        System.out.print(text);*/
 
-        Button b = (Button)v;
-        String buttonText = b.getText().toString();
+        Button DeleteButton = (Button) findViewById(R.id.DeleteButton);
+
+        DeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button Submit = (Button) findViewById(R.id.Submit);
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventObject newEvent = new EventObject("2","Mon","09:25AM");
+                finish();
+            }
+        });
+
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +53,4 @@ public class NewEventActivity extends AppCompatActivity {
             }
         });*/
     }
-
-
 }
